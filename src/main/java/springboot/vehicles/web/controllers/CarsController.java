52a2c463c2f0da58bb518f4757c3d1592a711066
Yavor <yavor300@gmail.com>
2021-01-04 09:07:23 +0200

    added data manually to the database and display information only through the repository layer

--- src/main/java/springboot/vehicles/web/controllers/CarsController.java
+++ src/main/java/springboot/vehicles/web/controllers/CarsController.java
@@ -1,15 +1,25 @@
 package springboot.vehicles.web.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import springboot.vehicles.repository.OfferRepository;
 
 @Controller
 @RequestMapping("/cars")
-public class CarsController {
+public class CarsController extends BaseController {
+    private final OfferRepository offerRepository;
+
+    @Autowired
+    public CarsController(OfferRepository offerRepository) {
+        this.offerRepository = offerRepository;
+    }
+
     @GetMapping("/all")
-    public ModelAndView getAllCars() {
-        return new ModelAndView("all");
+    public ModelAndView getAllCars(ModelAndView modelAndView) {
+        modelAndView.addObject("offers", offerRepository.findAll());
+        return super.view("all", modelAndView);
     }
 }
